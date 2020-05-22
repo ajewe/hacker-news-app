834e98a7930ba8bfab225729929d5954da89af04
ajewe <ajewe@utexas.edu>
2020-05-22 13:52:17 -0500

    Updates

---
+++ precache-manifest.c4fab6a05afedad4b3e247013f3e4cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c6f2ce358914cff546850c0f76ae0c9",
+    "url": "/hacker-news-app/index.html"
+  },
+  {
+    "revision": "fd5b8ed138b719e5c4d9",
+    "url": "/hacker-news-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36a9148c7a75719a61cb",
+    "url": "/hacker-news-app/static/js/2.a92d4df7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news-app/static/js/2.a92d4df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5b8ed138b719e5c4d9",
+    "url": "/hacker-news-app/static/js/main.debae7d6.chunk.js"
+  },
+  {
+    "revision": "262c4d9620b489ba1ff7",
+    "url": "/hacker-news-app/static/js/runtime-main.a09bd099.js"
+  }
+]);
